26545595f19b9979a68270e3147f861ea01903cd
AnishKhatri17 <anish.khatri.135@gmail.com>
2025-05-11 11:00:56 +0545

    Area of Circle program in C# programming

---
+++ Area_of_Circle/Area of Circle/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Area_of_Circle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double pi = 3.14, r, a;
+            Console.WriteLine("Enter the radius of a Circle :");
+            r = Convert.ToDouble(Console.ReadLine());
+            a = pi * r * r;
+            Console.WriteLine("The area of a circle is = " + a);
+            Console.ReadKey();
+        }
+    }
+}
