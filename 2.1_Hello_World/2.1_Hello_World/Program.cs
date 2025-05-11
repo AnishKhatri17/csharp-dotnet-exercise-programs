ff1582531e785523a90958addf3e82d0526fbdba
AnishKhatri17 <anish.khatri.135@gmail.com>
2025-05-11 10:57:42 +0545

    First C# program to print'Hello World'

---
+++ 2.1_Hello_World/2.1_Hello_World/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2._1_Hello_World
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World.\n This is my First Program in C# Programming ...");
+        }
+    }
+}
