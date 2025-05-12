a94af13b233c15ad1eb40934bccde666cf05e3b3
AnishKhatri17 <anish.khatri.135@gmail.com>
2025-05-12 10:29:57 +0545

    Area of rectangle example program in C# programming

---
+++ Area_of_Rectangle/Area of Rectangle/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Area_of_Rectangle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int l, b, a;
+            Console.WriteLine("Enter the length of rectangle : ");
+            l = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the breadth of rectangle : ");
+            b = Convert.ToInt32(Console.ReadLine());
+            a = l * b;
+            Console.WriteLine("The area of a rectangle is = "+a);
+        }
+    }
+}
