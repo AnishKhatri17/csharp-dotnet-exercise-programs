f7ec231426c31c6a66c13289c56f7af4882555f4
AnishKhatri17 <anish.khatri.135@gmail.com>
2025-05-12 17:44:20 +0545

    Fcatorial of a number in C# Programming

---
+++ Factorial/Factorial/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factorial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, fact = 1, i;
+            Console.Write("\n\nEnter the number you want the factorial of : ");
+            n = Convert.ToInt32(Console.ReadLine());
+                for(i=1; i<=n; i++)
+            {
+                fact = fact*i;
+            }
+            Console.WriteLine("\nThe factorial of a given number is = "+fact);
+        }
+    }
+}
