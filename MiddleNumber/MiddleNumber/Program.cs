ef3b7e597bd23a4d6f52d6411c3fa3556bf02345
AnishKhatri17 <anish.khatri.135@gmail.com>
2025-05-12 10:35:02 +0545

    c# program to find the middle number among three numbers

---
+++ MiddleNumber/MiddleNumber/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiddleNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c ;
+            Console.WriteLine("Enter first Number :");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter second Number :");
+            b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter third Number :");
+            c = Convert.ToInt32(Console.ReadLine());
+
+                if((a>b && a<c) || (a<b && a>c))
+            {
+                Console.WriteLine("Middle Number is : "  +a);
+            }
+                else if((b>c && b<a) || (b<c && b>a))
+            {
+                Console.WriteLine("Middle Number is : " +b);
+            }
+                else if((c>a && c<b) || (c<a && c>b))
+            {
+                Console.WriteLine("Middle Number is : "+ c);
+            }
+            else
+            {
+                Console.WriteLine("Case doesn't Match...");
+            }
+            Console.ReadKey();
+        }
+    }
+}
