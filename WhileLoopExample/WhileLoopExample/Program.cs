908652eb85d2783625ab180fe56c253ae843c62a
AnishKhatri17 <anish.khatri.135@gmail.com>
2025-05-12 10:31:28 +0545

    while loop example program in C# Programming

---
+++ WhileLoopExample/WhileLoopExample/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhileLoopExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i =1 ;
+            Console.WriteLine("The natural numbers from 1 to 100 ");
+                while(i <=100)
+            {
+                Console.WriteLine(i);
+                i++;
+            }
+        }
+    }
+}
