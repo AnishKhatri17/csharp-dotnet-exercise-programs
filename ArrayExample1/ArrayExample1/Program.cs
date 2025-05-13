2a32c33b1c03928bd488c8d535bc5abdbe0064cb
AnishKhatri17 <anish.khatri.135@gmail.com>
2025-05-13 12:06:50 +0545

    Array Example to display the names in the Array in C#

---
+++ ArrayExample1/ArrayExample1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayExample1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, i;
+            int[] a = new int[100];
+            Console.WriteLine("Enter the no. of Array Elements :(1-100) ");
+            n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the Elements of an Array : ");
+            for (i=0; i < n; i++)
+            {
+                a[i] = Convert.ToInt32(Console.ReadLine()); 
+               
+            }
+            Console.WriteLine("\n\nThe Array Elements Are : ");
+                for(i=0; i < n; i++)
+            {
+                Console.Write(" \n" + a[i]);
+            }
+                Console.ReadKey();  
+        }
+
+    }
+}
