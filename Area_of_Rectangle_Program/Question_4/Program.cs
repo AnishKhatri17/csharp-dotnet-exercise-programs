b3c5ccf856f7f67a8c402661bd13cf152ef8afda
AnishKhatri17 <anish.khatri.135@gmail.com>
2025-05-13 12:11:24 +0545

    C# Program to calculate the area of a rectangle

---
+++ Area_of_Rectangle_Program/Question_4/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int l, b, a;
+            Console.WriteLine("Enter length");
+            l= Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter breadth");
+            b= Convert.ToInt32(Console.ReadLine());
+            a = l * b;
+            Console.WriteLine("Area of Rectangle is : "+a);
+            Console.ReadKey();
+        }
+    }
+}
