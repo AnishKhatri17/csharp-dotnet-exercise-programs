a5cd4c6aa8ca1328e680af459879473d347ea622
AnishKhatri17 <anish.khatri.135@gmail.com>
2025-05-14 06:48:53 +0545

    C# Program to find the biggest among two numbers using the concept of class and objects in C#

---
+++ Biggest_Among_Two_using_class_object/Biggest_Among_Two_using_class_object/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biggest_Among_Two_using_class_object
+{
+    internal class Biggest
+    {
+       int x, y;
+       
+        public void Greatest()
+        {
+            Console.WriteLine("Enter First Number.");
+            x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Second Number.");
+            y = Convert.ToInt32(Console.ReadLine());
+            if (x > y)
+            {
+                Console.WriteLine("The biggest number is : "+x);
+            }
+            else
+            {
+                Console.WriteLine("\nThe biggest number is : " +y);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Biggest big = new Biggest();
+           // big.x = 100;
+           // big.y = 150;
+            big.Greatest();
+            Console.ReadKey();
+        }
+    }
+}
