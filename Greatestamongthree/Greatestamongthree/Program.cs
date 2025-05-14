acb6200cc03ef1758cb9f2ab1fa9df2f850d202a
AnishKhatri17 <anish.khatri.135@gmail.com>
2025-05-14 07:02:37 +0545

    C# program to find the greatest number among three numbers

---
+++ Greatestamongthree/Greatestamongthree/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Greatestamongthree
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c;
+            Console.Write("Enter the value of First Number :");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter the value of Second Number:");
+            b = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter the value of Third Number :");
+            c = Convert.ToInt32(Console.ReadLine());
+            if (a > b)
+            {
+                //Console.WriteLine("The Greatest Number is = " + a);
+                if (a > c)
+                {
+                    Console.WriteLine("The Greatest Number is = " + a);
+                }
+                else
+                {
+                    Console.WriteLine("The Greatest Number is = " + c);
+                }
+            }
+            else
+            {
+                if (b > c)
+                    Console.WriteLine("The Greatest number is = " + b);
+
+                else
+                {
+                    Console.WriteLine("The Greatest number is = " + c);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
