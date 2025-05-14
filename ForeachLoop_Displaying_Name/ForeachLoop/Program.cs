795a2f4be97d5c11c2fffe5608509b9f3a8a48c9
AnishKhatri17 <anish.khatri.135@gmail.com>
2025-05-14 07:00:01 +0545

    foreach Loop in C# simple program to display the names in the Array

---
+++ ForeachLoop_Displaying_Name/ForeachLoop/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForeachLoop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] arr = new string[5];
+             arr[0] = "Anish";
+             arr[1] = "Archana";
+             arr[2] = "Shirish";
+             arr[3] = "Junel";
+             arr[4] = "Bitisha";
+          
+                foreach(string day in arr)
+            {
+                Console.WriteLine(day);
+            }
+                    Console.ReadKey();  
+        }
+    }
+}
