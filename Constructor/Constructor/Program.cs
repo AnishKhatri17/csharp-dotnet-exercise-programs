aee2b6470c5f369b92970c705f89fd32fc86eba8
AnishKhatri17 <anish.khatri.135@gmail.com>
2025-05-14 06:56:09 +0545

    constructor example program in C#

---
+++ Constructor/Constructor/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor
+{
+    internal class Constructor
+    {
+        int a=5 , b=8; 
+         Constructor()
+        {
+            int c = a + b;
+            Console.WriteLine("The sum is : "+c);
+            Console.WriteLine("HELLOOOOOOOOOOOO");
+        }//default Constructor
+
+
+     
+        static void Main(string[] args)
+        {
+        Constructor c = new Constructor();
+
+            //c.Constructor();
+            Console.ReadKey();
+        }
+    }
+}
