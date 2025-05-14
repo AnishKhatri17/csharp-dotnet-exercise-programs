d5391e2338f64085188d7b423c0d39b75209d51c
AnishKhatri17 <anish.khatri.135@gmail.com>
2025-05-14 07:05:01 +0545

    C# program to input cost price and sales price from user and that calculates whether it is profit or loss

---
+++ ProfitorLoss/ProfitorLoss/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProfitorLoss
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int cost, sales, gain, loss;
+            Console.WriteLine("Enter the Cost Price of the item : ");
+            cost = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the Sales Price of the item : ");
+            sales = Convert.ToInt32(Console.ReadLine());
+            gain = sales - cost;
+            loss = cost - sales;
+                if(cost > sales)
+            {
+                Console.WriteLine("\n\nThere is a Loss of Rs."+loss);
+            }
+                else 
+            {
+                Console.WriteLine("\nThere is a Profit of Rs." +gain) ;
+            }
+            Console.ReadKey();
+        }
+    }
+}
