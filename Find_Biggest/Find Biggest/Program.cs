aca23674915457b9508b6b8f3e94a0a625f36e7b
AnishKhatri17 <anish.khatri.135@gmail.com>
2025-05-14 06:57:35 +0545

    C# Program to find the biggest number

---
+++ Find_Biggest/Find Biggest/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Find_Biggest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int first, second;
+            Console.WriteLine("Enter the value of First Number and Second Number : ");
+            Console.WriteLine("\n\n");
+            first = Convert.ToInt32(Console.ReadLine());
+            second = Convert.ToInt32(Console.ReadLine());
+                if (first > second)
+            {
+                Console.WriteLine("The greatest number is = " + first);
+            }
+                else
+            {
+                Console.WriteLine("The greatest number is = " + second);
+            }
+                Console.ReadKey();
+        }
+    }
+}
