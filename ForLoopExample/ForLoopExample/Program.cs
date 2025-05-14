d275ee01ef98b919b3d872c622e02e297f7c75ac
AnishKhatri17 <anish.khatri.135@gmail.com>
2025-05-14 07:00:52 +0545

    For Loop example program in C#

---
+++ ForLoopExample/ForLoopExample/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForLoopExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i=1;
+            Console.WriteLine("\n\nThe natural numbers from 1 to 100 is : \n\n");
+            for(i=1; i<=100; i++)
+            {
+                Console.WriteLine(i);
+            }
+                Console.ReadKey();
+        }
+    }
+}
