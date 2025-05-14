9d347307f403eea04aa0144b524a4125a0664b3b
AnishKhatri17 <anish.khatri.135@gmail.com>
2025-05-14 06:46:54 +0545

    C# program to find the greatest and smallest number in an Array

---
+++ ArrayGreatestAndSmallest/ArrayGreatestAndSmallest/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayGreatestAndSmallest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, n, greatest, smallest ;
+            int[] arr = new int[20];
+            Console.Write("\nEnter the number of Array Elements(1-20) : ");
+            n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("\nEnter the Array Elememts : ");
+                for(i = 0; i < n; i++)
+            {
+                arr[i]= Convert.ToInt32(Console.ReadLine());
+            }
+            greatest = arr[0];
+            smallest = arr[0];
+                for(i = 0; i < n; i++)
+            {
+                if (greatest < arr[i])
+                {
+                    greatest = arr[i];
+                }
+                    if(smallest > arr[i])
+                {
+                    smallest = arr[i];
+                }
+
+            }
+            Console.WriteLine("\n\nThe Greatest Number of an Array is : " + greatest);
+            Console.WriteLine("The Smallest Number of an Array is : " + smallest);
+            Console.ReadKey();
+        }
+    }
+}
