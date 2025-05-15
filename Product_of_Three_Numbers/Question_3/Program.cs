288745527f9649d6ef70f8090988a7bfd9c7cde5
AnishKhatri17 <anish.khatri.135@gmail.com>
2025-05-15 06:42:44 +0545

    C# program to product three numbers

---
+++ Product_of_Three_Numbers/Question_3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c, product;
+            Console.WriteLine("Enter first number");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter second number");
+            b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the third number");
+            c = Convert.ToInt32(Console.ReadLine());
+            product = a * b * c;
+            Console.WriteLine("Product of three number is" + product);
+            Console.ReadKey();
+        }
+    }
+}
