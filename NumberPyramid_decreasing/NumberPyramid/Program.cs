241915038b9a0d5a8edb4abe22d9262e21deae44
AnishKhatri17 <anish.khatri.135@gmail.com>
2025-05-15 06:36:34 +0545

    Number Pyramid based on the user input number in desccending order

---
+++ NumberPyramid_decreasing/NumberPyramid/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberPyramid
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, j, num;
+            Console.WriteLine("\nEnter the number to see the pyramid : ");
+            num = Convert.ToInt32(Console.ReadLine());
+                for(i=num; i>=1; i--) 
+            {
+                for (j = 1; j <= i; j++)
+                {
+                    Console.Write(j +"\t");
+                }
+                Console.WriteLine("\n");
+            }
+                Console.ReadKey();
+        }
+    }
+}
