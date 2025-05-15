084f7dfce44e402d4f2269564d050e2b933f4243
AnishKhatri17 <anish.khatri.135@gmail.com>
2025-05-15 06:38:05 +0545

    Finding the biggest number using the concept of objects in C#

---
+++ Object_Oriented_Biggest_Number/Object_Oriented/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Object_Oriented
+{
+    internal class Anish
+    {
+        public int Biggest(int x, int y)
+        {
+            int result;
+            if(x>y) 
+            {
+                result = x;
+            }
+            else 
+            {
+                result = y;
+            }
+            return result;
+        }
+        static void Main(string[] args)
+        {
+            Anish a1 = new Anish();
+            int first = 50;
+            int second = 45;
+            int output = a1.Biggest(first, second);
+           Console.WriteLine("The biggest number is : \n"+output);
+
+            Console.ReadKey();
+        }
+    }
+}
