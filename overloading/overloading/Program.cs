4f3c5e1585f4de9f50e55a239bf67a3ab05b3e42
AnishKhatri17 <anish.khatri.135@gmail.com>
2025-05-15 06:39:09 +0545

    Method overloading example program in C#

---
+++ overloading/overloading/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace overloading
+{
+    internal class Program
+    {
+        void display(int a)
+        {
+            Console.WriteLine("the value of a is: " + a);
+        }
+
+        void display(int a,int b)
+        {
+            Console.WriteLine("the value of a is:" + a+ "\n" + "the value of b is: "+b);
+                }
+        static void Main(string[] args)
+        {
+            // Example of Method Overloading in C# .....
+            Program obj = new Program();
+            obj.display(1);
+            obj.display(50, 60);
+            obj.display(55, 100);
+            Console.ReadLine();
+        }
+    }
+}
