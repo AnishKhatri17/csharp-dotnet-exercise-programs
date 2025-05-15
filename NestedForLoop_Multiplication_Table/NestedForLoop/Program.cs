d48a91856cf61f67f2ac64a938cdadf5add3caf4
AnishKhatri17 <anish.khatri.135@gmail.com>
2025-05-15 06:35:01 +0545

    Nested For loop using to demostrate Multiplication table in C#

---
+++ NestedForLoop_Multiplication_Table/NestedForLoop/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NestedForLoop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, i, j, r;
+            Console.Write("Enter the number you want multiplication tables for: ");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            for (i = 1; i <= n; i++)
+            {
+                Console.WriteLine($"Multiplication Table of {i}:");
+                for (j = 1; j <= 10; j++)
+                {
+                    r = i * j;
+                    Console.WriteLine($"{i} x {j} = {r}");
+                }
+                Console.WriteLine(); // Blank line after each table
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
+
