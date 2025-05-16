552031ae6ee07b9057be166c6af62a5bb6722ec3
AnishKhatri17 <anish.khatri.135@gmail.com>
2025-05-16 21:07:28 +0545

    C# program to demonstrate C# program to find out day of the week

---
+++ SwitchCase/SwitchCase/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwitchCase
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int day;
+            Console.WriteLine("\nEnter your choice from 1 - 7 to select day of the week : ");
+            day = Convert.ToInt32(Console.ReadLine());
+            switch (day)
+            {
+                
+                case 1:
+                    {
+                        Console.WriteLine("\n\n");
+                        Console.WriteLine("Today is Sunday.");
+                        break;
+                    }
+                case 2: 
+                    {
+                        Console.WriteLine("\n\n");
+                        Console.WriteLine("Today is Monday.");
+                        break;
+                    }
+                case 3:
+                    {
+                        Console.WriteLine("\n\n");
+                        Console.WriteLine("Today is Tuesday.");
+                        break;
+                    }
+                case 4:
+                    {
+                        Console.WriteLine("\n\n");
+                        Console.WriteLine("Today is Wednesday.");
+                        break;
+                    }
+                case 5:
+                    {
+                        Console.WriteLine("\n\n");
+                        Console.WriteLine("Today is Thursday.");
+                        break;
+                    }
+                case 6:
+                    {
+                        Console.WriteLine("\n\n");
+                        Console.WriteLine("Today is Friday.");
+                        break;
+                    }
+                case 7:
+                    {
+                        Console.WriteLine("\n\n");
+                        Console.WriteLine("Today is Saturday.");
+                        break;
+                    }
+                default :
+                    {
+                        Console.WriteLine("\n\n");
+                        Console.WriteLine("Invalid Choice !!!!!!");
+                         break;
+                    }
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
