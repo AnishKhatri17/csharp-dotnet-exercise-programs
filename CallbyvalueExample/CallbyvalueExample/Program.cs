a4bb5181983587b015cbd2eb3bf89a5cf6a4839e
AnishKhatri17 <anish.khatri.135@gmail.com>
2025-05-16 20:59:52 +0545

    Call by value example program in C#

---
+++ CallbyvalueExample/CallbyvalueExample/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallbyvalueExample
+{
+    public class Program
+    {
+            static void callbyvalue(int a)
+        {
+            Console.Write("The value of a is :"+a);
+        }
+        static void Main(string[] args)
+        {
+            callbyvalue(25);
+            Console.ReadKey();
+        }
+    }
+}
