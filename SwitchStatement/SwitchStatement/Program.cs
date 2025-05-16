af8c79f9129d6b12ad149c153f34a0a949818f27
AnishKhatri17 <anish.khatri.135@gmail.com>
2025-05-16 21:08:25 +0545

    Switch Statement another example program to display day of the week based on user input from 1 to 7

---
+++ SwitchStatement/SwitchStatement/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwitchStatement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int day;
+            Console.WriteLine("\nEnter your choice from 1 - 7 to select day of the week : ");
+            day = Convert.ToInt32(Console.ReadLine());
+            switch (day)
+            {
+
+                case 1:
+                    {
+                        Console.WriteLine("\n\n");
+                        Console.WriteLine("Today is Sunday. Click Enter to Exit");
+                        break;
+                    }
+                case 2:
+                    {
+                        Console.WriteLine("\n\n");
+                        Console.WriteLine("Today is Monday. Click Enter to Exit");
+                        break;
+                    }
+                case 3:
+                    {
+                        Console.WriteLine("\n\n");
+                        Console.WriteLine("Today is Tuesday. Click Enter to Exit");
+                        break;
+                    }
+                case 4:
+                    {
+                        Console.WriteLine("\n\n");
+                        Console.WriteLine("Today is Wednesday. Click Enter to Exit");
+                        break;
+                    }
+                case 5:
+                    {
+                        Console.WriteLine("\n\n");
+                        Console.WriteLine("Today is Thursday. Click Enter to Exit");
+                        break;
+                    }
+                case 6:
+                    {
+                        Console.WriteLine("\n\n");
+                        Console.WriteLine("Today is Friday. Click Enter to Exit");
+                        break;
+                    }
+                case 7:
+                    {
+                        Console.WriteLine("\n\n");
+                        Console.WriteLine("Today is Saturday. Click Enter to Exit");
+                        break;
+                    }
+                default:
+                    {
+                        Console.WriteLine("\n\n");
+                        Console.WriteLine("Invalid Choice !!!!!!");
+                        break;
+                    }  
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
