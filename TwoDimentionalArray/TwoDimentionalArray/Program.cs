61eab18a99cdf19577d4afaff047874a6e8f80b1
AnishKhatri17 <anish.khatri.135@gmail.com>
2025-05-16 21:10:57 +0545

    Two Dimentional Array example program demonstration in C# Programming

---
+++ TwoDimentionalArray/TwoDimentionalArray/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwoDimentionalArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, j;
+            int[,] a = new int[3, 2]
+            {
+                {1,2}, 
+                {1,2}, 
+                {1,2}
+            };
+
+
+            Console.WriteLine("\n\nThe elements in the two dimentional Array are : \n");
+                for(i=0; i<3; i++) 
+            {
+                for(j=0; j<2; j++)
+                {
+                    Console.Write(+a[i,j]);  
+                    Console.Write("\t");
+                }
+                Console.WriteLine("\n");    
+            }    
+                Console.ReadKey();  
+        }
+    }
+}
