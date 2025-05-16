8ca5dde523d9046db87628b42b5c58dafed24837
AnishKhatri17 <anish.khatri.135@gmail.com>
2025-05-16 21:01:11 +0545

    call by value greatest number example program in C#

---
+++ CallbyvalueGreatest/CallbyvalueGreatest/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallbyvalueGreatest
+{
+            public class Program
+              {
+        public void GreatestNumber(int first, int second, int third)
+        {
+            if (first > second)
+            {
+                if (first > third)
+                {
+                    Console.WriteLine("\n\nThe Greatest Number is : " + first);
+                }
+            }
+                    else if(second > third)
+            {
+                if (second > first) 
+                {
+                    Console.WriteLine("\n\nThe Greatest Number is : " + second);
+                }
+            }
+            else
+            {
+                Console.WriteLine("\n\nThe Greatest Number is : " + third);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Program obj = new Program();
+            obj.GreatestNumber(10, 15, 70);
+        }
+    }
+}
