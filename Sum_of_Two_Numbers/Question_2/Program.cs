c8efb30a017f40b8257890b4f292ebb6bf7d039c
AnishKhatri17 <anish.khatri.135@gmail.com>
2025-05-16 21:04:05 +0545

    Sum of two numbers example program in C#

---
+++ Sum_of_Two_Numbers/Question_2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question_2
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c;
+            Console.WriteLine("Enter the value of first number.");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the value of second number.");
+            b = Convert.ToInt32(Console.ReadLine());
+            c = a + b;
+            Console.WriteLine("The sum of two numbers is :"+c);
+
+            Console.ReadKey();
+
+        }
+    }
+}
